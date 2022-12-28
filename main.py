5a10d80a14416b0b91c4cd2fca94ea1971798dfc
Maha2230 <wmadhav85@gmail.com>
2022-12-28 19:29:55 +0530

    My third commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #this is switched master_branch to features_branch
-#this is switched  flipcard
+# my second commit
+# my third commit
 thislist = ["apple", "banana", "cherry", "apple", "cherry"]
 
 print(thislist)
 
+x = 5
+y = 10
+print(x + y)
